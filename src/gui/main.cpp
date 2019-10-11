8112db1039340e1edb6cc7f5698f3e1499fb4fa0
JJPPeters <j.peters.1@warwick.ac.uk>
2019-10-11 15:28:56 +0100

    Fixed log file size becoming too large in totally the right branch for this

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char *argv[]) {
     defaultConf.setGlobally(el::ConfigurationType::Filename, log_dir.toStdString());
     defaultConf.setGlobally(el::ConfigurationType::Format, "[%logger] %datetime (thread:%thread) %level - %func: %msg");
     defaultConf.setGlobally(el::ConfigurationType::ToStandardOutput, "false");
+    defaultConf.setGlobally(el::ConfigurationType::MaxLogFileSize, "1073741824");
 
     QSettings settings;
     if (!settings.contains("logging"))
