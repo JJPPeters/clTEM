52ba20c3cc27e46da9727be5629493e83392bf3e
JJPPeters <j.peters.1@warwick.ac.uk>
2020-05-24 13:08:15 +0100

    started playing with tab configurations

--- src/gui/frames/aberrationframe.cpp
+++ src/gui/frames/aberrationframe.cpp
@@ -22,33 +22,33 @@ AberrationFrame::AberrationFrame(QWidget *parent) :
     ui->edtStigMag->setValidator(pmValidator);
     ui->edtStigAng->setValidator(pmValidator);
 
-    ui->edtAperture->setValidator(pValidator);
-    ui->edtDelta->setValidator(pValidator);
-    ui->edtConverge->setValidator(pValidator);
-    ui->edtVoltage->setValidator(pValidator);
+//    ui->edtAperture->setValidator(pValidator);
+//    ui->edtDelta->setValidator(pValidator);
+//    ui->edtConverge->setValidator(pValidator);
+//    ui->edtVoltage->setValidator(pValidator);
 
-    ui->edtBeamTilt->setValidator(pmValidator);
-    ui->edtBeamAzimuth->setValidator(pmValidator);
+//    ui->edtBeamTilt->setValidator(pmValidator);
+//    ui->edtBeamAzimuth->setValidator(pmValidator);
 
     ui->edtDefocus->setUnits("nm");
     ui->edtSphere->setUnits("μm");
     ui->edtStigMag->setUnits("nm");
     ui->edtStigAng->setUnits("°");
 
-    ui->edtAperture->setUnits("mrad");
-    ui->edtDelta->setUnits("nm");
-    ui->edtConverge->setUnits("mrad");
-    ui->edtVoltage->setUnits("kV");
-
-    ui->edtBeamTilt->setUnits("mrad");
-    ui->edtBeamAzimuth->setUnits("°");
+//    ui->edtAperture->setUnits("mrad");
+//    ui->edtDelta->setUnits("nm");
+//    ui->edtConverge->setUnits("mrad");
+//    ui->edtVoltage->setUnits("kV");
+//
+//    ui->edtBeamTilt->setUnits("mrad");
+//    ui->edtBeamAzimuth->setUnits("°");
 
     // these connect to a slot that makes the text colour change if the value is zero
     // I don't want to disable 0 in the regex as people might want it as a leading character (e.g. 0.1)
-    connect(ui->edtAperture, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero);
-    connect(ui->edtDelta, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero);
-    connect(ui->edtConverge, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero);
-    connect(ui->edtVoltage, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero); // this also has a 'by name' slot
+//    connect(ui->edtAperture, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero);
+//    connect(ui->edtDelta, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero);
+//    connect(ui->edtConverge, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero);
+//    connect(ui->edtVoltage, &QLineEdit::textChanged, this, &AberrationFrame::checkEditZero); // this also has a 'by name' slot
 }
 
 AberrationFrame::~AberrationFrame()
@@ -102,14 +102,14 @@ void AberrationFrame::updateTextBoxes()
     ui->edtStigMag->setText(Utils_Qt::numToQString(p->C12.Mag / 10)); // nm
     ui->edtStigAng->setText(Utils_Qt::numToQString((180 / Constants::Pi) * p->C12.Ang)); // degrees
 
-    ui->edtDelta->setText(Utils_Qt::numToQString(p->Delta / 10)); // nm
-    ui->edtConverge->setText(Utils_Qt::numToQString(p->Alpha)); // mrad
-
-    ui->edtVoltage->setText(Utils_Qt::numToQString(p->Voltage)); // kV
-    ui->edtAperture->setText(Utils_Qt::numToQString(p->Aperture)); // mrad
-
-    ui->edtBeamTilt->setText(Utils_Qt::numToQString(p->BeamTilt)); // mrad
-    ui->edtBeamAzimuth->setText(Utils_Qt::numToQString((180 / Constants::Pi) * p->BeamAzimuth)); // degrees
+//    ui->edtDelta->setText(Utils_Qt::numToQString(p->Delta / 10)); // nm
+//    ui->edtConverge->setText(Utils_Qt::numToQString(p->Alpha)); // mrad
+//
+//    ui->edtVoltage->setText(Utils_Qt::numToQString(p->Voltage)); // kV
+//    ui->edtAperture->setText(Utils_Qt::numToQString(p->Aperture)); // mrad
+//
+//    ui->edtBeamTilt->setText(Utils_Qt::numToQString(p->BeamTilt)); // mrad
+//    ui->edtBeamAzimuth->setText(Utils_Qt::numToQString((180 / Constants::Pi) * p->BeamAzimuth)); // degrees
 }
 
 void AberrationFrame::updateAberrations()
@@ -126,28 +126,28 @@ void AberrationFrame::updateAberrations()
     double C10 = ui->edtDefocus->text().toDouble() * 10; // Angstrom
     double C30 = ui->edtSphere->text().toDouble() * 10000; // Angstrom
 
-    double delt = ui->edtDelta->text().toDouble() * 10; // Angstrom
-    double apert = ui->edtAperture->text().toDouble(); // mrad
-    double conv = ui->edtConverge->text().toDouble(); // mrad
-    double volt = ui->edtVoltage->text().toDouble(); // kV
+//    double delt = ui->edtDelta->text().toDouble() * 10; // Angstrom
+//    double apert = ui->edtAperture->text().toDouble(); // mrad
+//    double conv = ui->edtConverge->text().toDouble(); // mrad
+//    double volt = ui->edtVoltage->text().toDouble(); // kV
 
     double C12m = ui->edtStigMag->text().toDouble() * 10; // Angstrom
     double C12a = ui->edtStigAng->text().toDouble() * Constants::Pi / 180; // radians
 
-    double beam_tilt = ui->edtBeamTilt->text().toDouble(); // mrad
-    double beam_azimuth = ui->edtBeamAzimuth->text().toDouble() * Constants::Pi / 180; // radians
+//    double beam_tilt = ui->edtBeamTilt->text().toDouble(); // mrad
+//    double beam_azimuth = ui->edtBeamAzimuth->text().toDouble() * Constants::Pi / 180; // radians
 
-    params->Voltage = volt;
-    params->Aperture = apert;
-    params->Delta = delt;
-    params->Alpha = conv;
+//    params->Voltage = volt;
+//    params->Aperture = apert;
+//    params->Delta = delt;
+//    params->Alpha = conv;
 
     params->C10 = C10;
     params->C12 = ComplexAberration(C12m, C12a);
     params->C30 = C30;
 
-    params->BeamTilt = beam_tilt;
-    params->BeamAzimuth = beam_azimuth;
+//    params->BeamTilt = beam_tilt;
+//    params->BeamAzimuth = beam_azimuth;
 }
 
 void AberrationFrame::on_edtVoltage_textChanged(const QString &arg1) {
@@ -155,6 +155,6 @@ void AberrationFrame::on_edtVoltage_textChanged(const QString &arg1) {
     if (Main == nullptr)
         throw std::runtime_error("Error connecting aberration frame to main window.");
 
-    double volt = ui->edtVoltage->text().toDouble();
-    Main->updateVoltageMrad( volt );
+//    double volt = ui->edtVoltage->text().toDouble();
+//    Main->updateVoltageMrad( volt );
 }
---
+++ src/gui/frames/sourceframe.h
@@ -0,0 +1,40 @@
+#ifndef SOURCEFRAME_H
+#define SOURCEFRAME_H
+
+#include <QWidget>
+#include <mainwindow.h>
+
+namespace Ui {
+class SourceFrame;
+}
+
+class SourceFrame : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SourceFrame(QWidget *parent = 0);
+
+    ~SourceFrame();
+
+//    void assignMainWindow(MainWindow* m) {Main = m; updateTextBoxes();}
+
+//    void updateAberrations();
+
+//private slots:
+//    void checkEditZero(QString dud);
+
+//    void on_btnMore_clicked();
+
+//    void on_edtVoltage_textChanged(const QString &arg1);
+
+//public slots:
+//    void updateTextBoxes();
+
+private:
+    Ui::SourceFrame *ui;
+
+    MainWindow* Main;
+};
+
+#endif // SOURCEFRAME_H
